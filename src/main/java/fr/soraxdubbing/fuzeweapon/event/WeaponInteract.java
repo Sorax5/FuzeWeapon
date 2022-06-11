8b338a9030fea257c6912ac64118e1774334e487
SoraxDubbing <52173496+Sorax5@users.noreply.github.com>
2022-06-11 20:14:15 +0200

    Structure Interaction RIGHT_CLICK

--- src/main/java/fr/soraxdubbing/fuzeweapon/event/WeaponInteract.java
+++ src/main/java/fr/soraxdubbing/fuzeweapon/event/WeaponInteract.java
@@ -2,13 +2,17 @@ package fr.soraxdubbing.fuzeweapon.event;
 
 import fr.soraxdubbing.fuzeweapon.FuzeWeapon;
 import fr.soraxdubbing.fuzeweapon.WeaponEnum;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class WeaponInteract implements Listener {
 
@@ -20,9 +24,53 @@ public class WeaponInteract implements Listener {
 
     @EventHandler
     public void onRightClick(PlayerInteractEvent event){
-        if(event.hasItem()){
-
+        ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
+        if(item.getType() != Material.AIR && (event.getAction().equals(Action.RIGHT_CLICK_AIR) ||event.getAction().equals(Action.RIGHT_CLICK_BLOCK))){
+            if(this.weaponList.containsValue(item)){
+                WeaponEnum weaponType = null;
+                for (Map.Entry<WeaponEnum,ItemStack> entry : weaponList.entrySet()){
+                    if(entry.getValue().equals(item)){
+                        weaponType = entry.getKey();
+                    }
+                }
+                switch (Objects.requireNonNull(weaponType)){
+                    case MITRAILLETTE:
+                        MitrailletteEffect();
+                        break;
+                    case INVOCATEUR:
+                        InvocateurEffect();
+                        break;
+                    case POSEIDON:
+                        PoseidonEffect();
+                        break;
+                    case LASER:
+                        LaserEffect();
+                        break;
+                    case FISH_SHOOTER:
+                        FishShooterEffect();
+                        break;
+                }
+            }
         }
+    }
+
+    private void MitrailletteEffect(){
+
+    }
+
+    private void InvocateurEffect(){
+
+    }
+
+    private void PoseidonEffect(){
+
+    }
+
+    private void LaserEffect(){
+
+    }
+
+    private void FishShooterEffect(){
 
     }
 }
